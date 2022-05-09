97be18d02de00df5b53ca2022166743cc2e7aa58
Everson Mariano <mariano.computacao@gmail.com>
2022-05-08 21:09:15 -0300

    Versao final

--- Back-end/bankline-api/src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ Back-end/bankline-api/src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -7,30 +7,20 @@ import javax.persistence.Embeddable;
 public class Conta {
     @Column(name = "conta_numero")
     private Long numero;
+
     @Column(name = "conta_saldo")
     private Double saldo;
-
-    public Conta() {
-    }
-
-    public Conta(Long numero, Double saldo) {
-        this.numero = numero;
-        this.saldo = saldo;
-    }
-
     public Long getNumero() {
         return numero;
     }
-
     public void setNumero(Long numero) {
         this.numero = numero;
     }
-
     public Double getSaldo() {
         return saldo;
     }
-
     public void setSaldo(Double saldo) {
         this.saldo = saldo;
     }
+
 }
